84c1e20d9c723555e84549446e242275a9465e7e
eveminyoso <eveminyoso@gmail.com>
2023-10-31 13:11:59 -0700

    lists.h

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
+
+#endif
