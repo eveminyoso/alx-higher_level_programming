72187f5d03edd33347f91f9377f2484fb6013a33
eveminyoso <eveminyoso@gmail.com>
2023-10-31 13:48:49 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(99):
     if i < 10:
-        print("{:2d}".format(i), end=", ")
-    print("{:d}".format(99)
+        print(str(i).format(), end=", ")
+    else:
+        print(str().format(99)
