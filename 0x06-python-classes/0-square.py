1d596ea258fc32b7487fe57b6be6a77f6d458397
eveminyoso <eveminyoso@gmail.com>
2023-11-21 13:54:57 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""An empty square model"""
+
+
+class Square:
+    """
+    This is a square class
+
+    Attributes:
+    (None)
+
+    Methods:
+    (None)
+    """
+    pass
