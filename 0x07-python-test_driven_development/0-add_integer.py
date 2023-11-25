b9ce571d48a6f16a58fe686c0b8d16e5081358fa
eveminyoso <eveminyoso@gmail.com>
2023-11-25 14:07:03 -0800

    test_driven

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for an addition function
+>>> add_integer(1)
+99
+"""
+
+
+def add_integer(a, b=98):
+        """
+            Returns a + b
+        """
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        a = int(a)
+        b = int(b)
+        return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod("./tests/0-add_integer.tt")
