7f62242d175ad6ff362b38e1067f521ad8a14d8b
eveminyoso <eveminyoso@gmail.com>
2023-11-28 12:38:29 -0800

    Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(board, row, N):
+    if row == N:
+        # All queens are placed, print the solution
+        print([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_nqueens(board, row + 1, N)
+
+def nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solve_nqueens(board, 0, N)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    nqueens(sys.argv[1])
