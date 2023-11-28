6f0311961e3930d8421015f40be671f532efb898
eveminyoso <eveminyoso@gmail.com>
2023-11-28 12:22:59 -0800

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Locked Class """
+
+
+class LockedClass:
+    """
+    A simple example of locked object
+    """
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        pass
