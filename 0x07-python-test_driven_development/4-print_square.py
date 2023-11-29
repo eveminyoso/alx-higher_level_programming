95f19ed254f3d49f885a07f4f9fb79cf73f8e648
eveminyoso <eveminyoso@gmail.com>
2023-11-29 12:22:31 -0800

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,25 +8,25 @@ Module for printing a square
 ####
 """
 
+
 def print_square(size):
-        """
-        Prints a square of size
+    """
+    Prints a square of size
+    Parameters:
+    size(int) - length of the square
 
-        Parameters:
-        size(int) - length of the square
-
-        prints - the square
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be an integer")
-        for _ in range(size):
-            print("#" * size)
+    prints - the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for _ in range(0, size):
+        print("#" * size)
 
 
-            if __name__ == "__main__":
-                import doctest
-                doctest.testmod("./tests/4-print_square.txt")
+        if __name__ == "__main__":
+            import doctest
+            doctest.testmod("./tests/4-print_square.txt")
