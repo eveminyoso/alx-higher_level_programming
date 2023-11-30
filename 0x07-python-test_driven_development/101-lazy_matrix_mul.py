76120ab24586153a6265480937d26d3909ba660a
eveminyoso <eveminyoso@gmail.com>
2023-11-30 03:50:10 -0800

    CPython

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
 """
-Module for matrix multiplication
-using numpy
+Module using numpy module for matrix multi
 """
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    numpy module for multiplitcation
+    Does matrix multiplication using numpy module
+
+    parameters:
+    m_a - first matrix
+    m_b - second matrix
     """
     a = np.array(m_a)
     b = np.array(m_b)
     if not m_a or not any(m_a):
-        raise ValueError("m_a can't be empty")
+        raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
     if not m_b or not any(m_b):
-        raise ValueError("m_b can't be empty")
+        raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
     if not m_a or not m_b or not any(m_a) or not any(m_b):
         raise ValueError("m_a can't be empty and m_b can't be empty")
     if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
     if not all(isinstance(element, (int, float)) for row in m_a for element in row):
-        raise TypeError("m_a should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
     if not all(isinstance(element, (int, float)) for row in m_b for element in row):
-        raise TypeError("m_b should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("Each row of m_a must be of the same size")
+        raise TypeError("setting an array element with a sequence.")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("Each row of m_b must be of the same size")
+        raise TypeError("setting an array element with a sequence.")
     if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+        raise ValueError("shapes (2,3) and (2,2) not aligned: 3 (dim 1) != 2 (dim 0)")
 
     result = np.dot(a, b)
 
     return result
+
 
 if __name__ == "__main__":
     import doctest
