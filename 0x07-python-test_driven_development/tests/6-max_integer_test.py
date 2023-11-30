3e3900ff32e810b67620db26cc9b93bb776c59a5
eveminyoso <eveminyoso@gmail.com>
2023-11-30 03:41:12 -0800

    CPython

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxIntegerFunction(unittest.TestCase):
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-    If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-            i += 1
-            return result
+    def test_regular_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -2, -8]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 5, 2, -8]), 5)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_single_element_list(self):
+        self.assertEqual(max_integer([42]), 42)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([1.5, 2.5, 1.0, 0.5]), 2.5)
+
+    def test_strings(self):
+        self.assertEqual(max_integer(["apple", "banana", "orange"]), "orange")
+
+    def test_mixed_types(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, "two", 3])
+
+if __name__ == '__main__':
+    unittest.main()
