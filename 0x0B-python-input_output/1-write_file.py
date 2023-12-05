55bfd39e9f6a3a6ac90403787bc7266f84cb7ad3
eveminyoso <eveminyoso@gmail.com>
2023-12-05 10:53:18 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        len_ = f.write(text)
+
+    return len_
