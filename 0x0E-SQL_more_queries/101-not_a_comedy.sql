250f16be3b0564013efe77ad1567280c3b061197
eveminyoso <eveminyoso@gmail.com>
2024-01-18 12:18:33 -0800

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,16 +1,9 @@
--- Get shows with the genre Comedy
-CREATE TEMPORARY TABLE comedy_shows_temp AS
-  SELECT DISTINCT tv_shows.id
-  FROM tv_shows
-  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-  JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-  WHERE tv_genres.name = 'Comedy';
+-- Get the genre ID for Comedy
+SET @comedy_genre_id := (SELECT id FROM tv_genres WHERE name = 'Comedy');
 
 -- List shows without the genre Comedy
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (SELECT id FROM comedy_shows_temp)
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL OR tv_show_genres.genre_id != @comedy_genre_id
 ORDER BY tv_shows.title ASC;
-
--- Clean up temporary table
-DROP TEMPORARY TABLE IF EXISTS comedy_shows_temp;
