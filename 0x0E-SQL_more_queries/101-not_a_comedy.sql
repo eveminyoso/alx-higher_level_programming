61ce801f5af8f56897dc4b79997e5a21598569d9
eveminyoso <eveminyoso@gmail.com>
2024-01-18 20:32:54 -0800

    fixed advanced

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,12 @@
--- Get the genre ID for Comedy
-SET @comedy_genre_id := (SELECT id FROM tv_genres WHERE name = 'Comedy');
-
--- List shows without the genre Comedy
-SELECT DISTINCT tv_shows.title
+-- lists all shows without  genre Comedy in the database hbtn_0d_tvshows
+-- uses  database to list all rows not linked to one row
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL OR tv_show_genres.genre_id != @comedy_genre_id
-ORDER BY tv_shows.title ASC;
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,7 @@
--- List shows by their rating sum
-SELECT TV_SHOWS.TITLE, SUM(TV_SHOW_RATINGS.RATING) AS RATING_SUM
-FROM TV_SHOWS
-JOIN TV_SHOW_RATINGS ON TV_SHOWS.ID = TV_SHOW_RATINGS.SHOW_ID
-GROUP BY TV_SHOWS.ID, TV_SHOWS.TITLE
-ORDER BY RATING_SUM DESC;
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+-- lists all rows of  table by the sum of a linked row
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
