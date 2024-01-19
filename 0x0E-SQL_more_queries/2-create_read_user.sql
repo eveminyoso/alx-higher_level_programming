94b58181cb899746698643fdc86e5ce90df5a25d
eveminyoso <eveminyoso@gmail.com>
2024-01-18 20:37:48 -0800

    fixed task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,3 +7,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating user if does not exists and grants privileges
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT  ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
