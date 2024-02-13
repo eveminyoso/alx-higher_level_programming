41e64e0767b9ac0356239764062621d6feeed35a
eveminyoso <eveminyoso@gmail.com>
2024-02-13 12:03:24 -0800

    3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,10 +4,3 @@ if (first) {
 	  console.log(first);
 } else {
 	  console.log('No argument');
-}#!/usr/bin/node
-const first = process.argv[2];
-if (first) {
-	  console.log(first);
-} else {
-	  console.log('No argument');
-}
