cecdbd9d26b0b10fc3d67f9dc25d83e7170c68a6
eveminyoso <eveminyoso@gmail.com>
2024-02-13 07:43:25 -0800

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (n) {
+	  if (n === 0 || n === 1) {
+		      return 1;
+		    }
+	  return n * factorial(n - 1);
+}
+const arg1 = process.argv[2];
+const n = parseInt(arg1);
+if (!isNaN(n)) {
+	  const result = factorial(n);
+	  console.log(result);
+} else {
+	  console.log(1);
+}
