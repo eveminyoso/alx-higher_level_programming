ba9872a620e046170db0f481cafab7fc8e5e0a60
eveminyoso <eveminyoso@gmail.com>
2024-02-13 12:54:15 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const first = process.argv[2];
 if (first) {
-	  console.log(first);
+	console.log(first);
 } else {
-	  console.log('No argument');
+	console.log('No argument');
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const arg1 = process.argv[2];
-if (isNaN(parseInt(arg1, 10))) {
-	  console.log('Not a number');
-} else {
-	  console.log('My number: ' + parseInt(arg1, 10));
-}
