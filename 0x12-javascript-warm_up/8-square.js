15839414207387e9c848ba636e70dfbde6e96d21
eveminyoso <eveminyoso@gmail.com>
2024-02-14 13:31:15 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,14 @@
-t arg1 = process.argv[2];
+#!/usr/bin/node
+const arg1 = process.argv[2];
 const size = parseInt(arg1);
 if (!isNaN(size)) {
-  for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-        row += 'X';
-	  }
-   console.log(row);
-  }
+	  for (let i = 0; i < size; i++) {
+		      let row = '';
+		      for (let j = 0; j < size; j++) {
+			            row += 'X';
+			          }
+		      console.log(row);
+		    }
 } else {
-  console.log('Missing size');
+	  console.log('Missing size');
 }
