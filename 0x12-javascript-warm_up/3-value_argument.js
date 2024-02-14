0be71a515e0f7583ed6fc0e391ef90a8c3f7fb45
eveminyoso <eveminyoso@gmail.com>
2024-02-14 12:38:38 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-function exampleFunction(arg1, arg2, arg3) {
-	if (arg1) {
-		console.log('Argument 1 is true');
-	} else if (arg2) {
-		console.log('Argument 2 is true');
-	} else {
-		console.log('Argument 3 is true');
-	}
+const first = process.argv[2];
+if (first) {
+	console.log(first);
+} else {
+	console.log('No argument');
 }
