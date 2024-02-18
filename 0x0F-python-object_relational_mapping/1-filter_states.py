847ba73468031004bcc2840bc0e6c408a1415b36
eveminyoso <eveminyoso@gmail.com>
2024-02-18 13:24:30 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
+"""
+listing according to science
+"""
 import MySQLdb
 import sys
 
 
-def list_states_with_n(username, password, database):
-    try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database)
-        cursor = db.cursor()
-        cursor.execute(
-                "SELECT * FROM states "
-                "WHERE name LIKE 'N%' ORDER BY states.id ASC")
-
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        db.close()
-
-    except Exception as e:
-        print("error")
+def list_cases(username, password, database):
+    """"list view wothout restrications"""
+    db = MySQLdb.connect(
+            user=username,
+            password=password,
+            database=database,
+            host="localhost",
+            port=3306
+            )
+    query = db.cursor()
+    query.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' "
+            "ORDER BY states.id ASC"
+            )
+    result = query.fetchall()
+    for i in result:
+        print(i)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Check if arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>")
+        print("Usage: ./executable.py <username> <password> <database>")
         sys.exit(1)
-
-    user, pwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    list_states_with_n(user, pwd, db)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_cases(username, password, database)
