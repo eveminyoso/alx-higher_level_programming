d5a3cbf3c1d2a459bcb41fd14b868790e8b50854
eveminyoso <eveminyoso@gmail.com>
2024-02-18 12:26:08 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,37 +7,40 @@ import sys
 
 
 def list_with_args(user, pwd, db, state):
-        """
-        params:
-        -> username: the root user
-        -> password: the root password
-        -> database: hbtn_0e_0_usa
-        -> state: the state to list from our db
-        """
-        db = MySQLdb.connect(
-                user=user,
-                password=pwd,
-                database=db,
-                host="localhost",
-                port=3306)
-        query = db.cursor()
-        query.execute(
-                "SELECT states.id, states.name "
-                "FROM states WHERE name = '{}' "
-                "ORDER BY states.id ASC".format(state)
-                )
-        result = query.fetchall()
-
-        if result:
-            for i in result:
-                print(i)
-                db.close()
+    """
+    params:
+    -> username: the root user
+    -> password: the root password
+    -> database: hbtn_0e_0_usa
+    -> state: the state to list from our db
+    """
+    db = MySQLdb.connect(
+            user=user,
+            password=pwd,
+            database=db,
+            host="localhost",
+            port=3306)
+    query = db.cursor()
+    query.execute(
+            "SELECT states.id, states.name "
+            "FROM states WHERE name = '{}' "
+            "ORDER BY states.id ASC".format(state)
+            )
+    result = query.fetchall()
 
 
-                if __name__ == '__main__':
-                    # validates the arguments
-                    if len(sys.argv) != 5:
-                        print("Usage: exec.py <user> <passwd> <db> <state>")
-                        sys.exit(1)
-                        user, pwd, db, state = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-                        list_with_args(user, pwd, db, state)
+if result:
+    for i in result:
+        print(i)
+        db.close()
+
+if __name__ == '__main__':
+    # validates the arguments
+    if len(sys.argv) != 5:
+        print("Usage: exec.py <user> <passwd> <db> <state>")
+        sys.exit(1)
+        user, pwd, db, state = sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+        sys.argv[4]
+        list_with_args(user, pwd, db, state)
