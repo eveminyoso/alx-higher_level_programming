140d51fbe1256e4e63f4fb690b0967e1f91676a1
eveminyoso <eveminyoso@gmail.com>
2024-02-18 12:15:37 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,25 +2,31 @@
 import MySQLdb
 import sys
 
+
 def list_states_with_n(username, password, database):
-        try:
-            db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-            cursor = db.cursor()
-            query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-            cursor.execute(query)
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database)
+        cursor = db.cursor()
+        cursor.execute(
+                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
-            results = cursor.fetchall()
-            for row in results:
-                print(row)
-                db.close()
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+            db.close()
 
-        except Exception as e:
-            print("Error:", e)
-            if __name__ == "__main__":
-            if len(sys.argv) != 4:
-                print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-                sys.exit(1)
+    except Exception as e:
+        print("Error:", e)
+    if __name__ == "__main__":
+        if len(sys.argv) != 4:
+            print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+            sys.exit(1)
 
-                username, password, database = sys.argv[1:]
+            username, password, database = sys.argv[1:]
 
-                list_states_with_n(username, password, database)
+            list_states_with_n(username, password, database)
