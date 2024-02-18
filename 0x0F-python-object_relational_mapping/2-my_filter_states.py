472aa5e4dc228d971a06cad856d8c9f2b9967234
eveminyoso <eveminyoso@gmail.com>
2024-02-18 12:27:23 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -42,5 +42,6 @@ if __name__ == '__main__':
         user, pwd, db, state = sys.argv[1],
         sys.argv[2],
         sys.argv[3],
-        sys.argv[4]
+        sys.argv[4],
+
         list_with_args(user, pwd, db, state)
