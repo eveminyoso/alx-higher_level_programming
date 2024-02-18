6f798a103c247b37a13212aefe05b64e17bb0c6d
eveminyoso <eveminyoso@gmail.com>
2024-02-18 11:01:25 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,18 +12,20 @@ def list_states(username, password, database):
     all_items.execute(
             "SELECT * FROM states "
             "ORDER BY states.id ASC"
+            )
 
-            result = all_items.fetchall()
-            for i in result:
-            print(i)
+    result = all_items.fetchall()
+    for i in result:
+        print(i)
 
-            db.close()
+        db.close()
 
 
-            if __name__ == '__main__':
+
+        if _name_ == '_main_':
             # Check if all arguments are provided
             if len(sys.argv) != 4:
-            print("Usage: python_script file <username> <passwd> <database>")
-            sys.exit(1)
-            username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-            list_states(username, password, database)
+                print("Usage: python_script file <username> <passwd> <database>")
+                sys.exit(1)
+                username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+                list_states(username, password, database)
