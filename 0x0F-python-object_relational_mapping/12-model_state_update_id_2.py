1f759aaa97211311d73f1e20a5140c9951d5b7ce
eveminyoso <eveminyoso@gmail.com>
2024-02-20 11:28:44 -0800

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+updating record
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessinmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    # connect to db and manipulate
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    condition = 2
+    new = {'name': 'New Mexico'}
+    result = session.query(State) \
+        .filter_by(id=condition) \
+        .update(new)
+    session.commit()
+    session.close()
