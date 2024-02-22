a60ad859702a682a88cfd285425af069ae95d7c3
eveminyoso <eveminyoso@gmail.com>
2024-02-21 16:40:47 -0800

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Model city
+"""
+import sqlalchemy
+from model_state import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    model definition
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+
+state = relationship("State", back_populates="cities")
